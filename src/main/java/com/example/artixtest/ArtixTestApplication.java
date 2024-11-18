b31bca5b570d77a8eb54c60c0785ea90dd4bf48b
defiigos <igorkoxd@gmail.com>
2024-11-18 23:50:27 +0700

    init commit

---
+++ src/main/java/com/example/artixtest/ArtixTestApplication.java
@@ -0,0 +1,13 @@
+package com.example.artixtest;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ArtixTestApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ArtixTestApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/artixtest/ArtixTestApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.artixtest;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ArtixTestApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
